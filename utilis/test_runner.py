f89b5ca80b969a68760d9e26a17fa2da1619d390
ritika <stharitika0@gmail.com>
2026-01-22 20:54:20 +0545

    CICD pipeline

--- utilis/test_runner.py
+++ utilis/test_runner.py
@@ -8,6 +8,8 @@ def create_driver():
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless=new") #UI (browser window) bina Chrome run garcha. Browser open hunna. Background ma automation chalcha
+
 
     service = Service(
         "C:\\Users\\Sriza\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"
